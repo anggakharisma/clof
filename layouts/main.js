29322ed17356e7cf7cf745ef0383923e02de835d
anggakharisma <anggaputrakharisma@gmail.com>
2019-05-21 06:51:20 +0700

    Adding item to cart is working properly now

--- layouts/main.js
+++ layouts/main.js
@@ -1,55 +1,55 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment } from "react";
 import Header from "../components/Header";
-import Footer from "../components/Footer"
+import Footer from "../components/Footer";
 import Head from "next/head";
 import Cart from "../components/Cart";
 
 function Main({ children, title = "CLOF | Clothing Store" }) {
-    return (
-      <Fragment>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>{title}</title>
-          <link
-            href="https://fonts.googleapis.com/css?family=Dosis:700|Nunito:300,400,600,700,800"
-            rel="stylesheet"
-          />
-        </Head>
-        <style jsx global>{`
-          * {
-            box-sizing: border-box;
-          }
-          body {
-            font-family: "Nunito", san-serif;
-          }
-          body,
-          html {
-            margin: 0;
-            padding: 0;
-            min-height: 100%;
-            width: 100%;
-            height: 100%;
-          }
-          main {
-            max-width: 95%;
-            margin: 0 auto;
-            margin-top: 64px;
-            min-height: 100%;
-          }
-          img {
-            max-width: 100%;
-          }
-          #__next {
-            min-height: 100%;
-            height: 100%;
-          }
-        `}</style>
-        <Header />
-        <Cart />
-        <main>{children}</main>
-        <Footer />
-      </Fragment>
-    )
-};
+  return (
+    <Fragment>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{title}</title>
+        <link
+          href="https://fonts.googleapis.com/css?family=Dosis:700|Nunito:300,400,600,700,800"
+          rel="stylesheet"
+        />
+      </Head>
+      <style jsx global>{`
+        * {
+          box-sizing: border-box;
+        }
+        body {
+          font-family: "Nunito", san-serif;
+        }
+        body,
+        html {
+          margin: 0;
+          padding: 0;
+          min-height: 100%;
+          width: 100%;
+          height: 100%;
+        }
+        main {
+          max-width: 95%;
+          margin: 0 auto;
+          margin-top: 64px;
+          min-height: 100%;
+        }
+        img {
+          max-width: 100%;
+        }
+        #__next {
+          min-height: 100%;
+          height: 100%;
+        }
+      `}</style>
+      <Header />
+      <Cart />
+      <main>{children}</main>
+      <Footer />
+    </Fragment>
+  );
+}
 
 export default Main;
--- useAppState.js
+++ useAppState.js
@@ -1,43 +1,56 @@
-import { useState, useEffect, useMemo, useContext, createContext } from 'react';
+import { useState, useEffect, useMemo, useContext, createContext } from "react";
 
 const AppContext = createContext({});
 
 const useAppState = () => {
-    let initialState = {
-        cartItems: [],
-        showCart: false
-    }
+  let initialState = {
+    cartItems: [],
+    showCart: false
+  };
 
-    useEffect(() => {
-        initialState.cartItems = JSON.parse(localStorage.getItem('cart')) || [];
-    }, []);
+  useEffect(() => {
+    initialState.cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+  }, []);
 
-    const [state, setState] = useState(initialState);
+  const [state, setState] = useState(initialState);
 
-    const actions = useMemo(() => getActions(setState), [setState]);
+  const actions = useMemo(() => getActions(setState), [setState]);
 
-    return { state, actions };
-}
+  return { state, actions };
+};
 
 const getActions = setState => ({
-    getCart: () => {
-        return JSON.parse(localStorage.getItem('cart')) || [];
-    },
-    toggleCart: () => {
-        setState(state => ({ ...state, showCart: !state.showCart }));
-    },
-    addToCart: (item) => {
-        setState(state => {
-            localStorage.setItem('cart', JSON.stringify([...state.cartItems, item]));
-            return {...state, showCart: true, cartItems: [...state.cartItems, item] }
-        })
-    }
-
+  getCart: () => {
+    return JSON.parse(localStorage.getItem("cart")) || [];
+  },
+  toggleCart: () => {
+    setState(state => ({ ...state, showCart: !state.showCart }));
+  },
+  addToCart: item => {
+    setState(state => {
+      let cartItemFound = state.cartItems.find(
+        cartItem => item._id === cartItem._id
+      );
+      if (!cartItemFound) {
+        let newCart = [...state.cartItems, { ...item, qty: 1 }];
+        localStorage.setItem("cart", JSON.stringify(newCart));
+        return { ...state, showCart: true, cartItems: newCart };
+      }
+      let newCart = state.cartItems.map(cartItem => {
+        console.log(cartItem.qty);
+        return cartItem._id === item._id
+          ? { ...cartItem, qty: ++cartItem.qty }
+          : cartItem;
+      });
+      console.log(newCart);
+      localStorage.setItem("cart", JSON.stringify(newCart));
+      return { ...state, showCart: true, cartItems: newCart };
+    });
+  }
 });
 
 const useAppContext = () => {
-    return useContext(AppContext);
-}
-
+  return useContext(AppContext);
+};
 
 export { useAppState, AppContext, useAppContext };
