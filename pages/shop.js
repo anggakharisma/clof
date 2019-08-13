ca935492d352bed7bfcae447fb3feb6f5a6e7220
anggakharisma <anggaputrakharisma@gmail.com>
2019-08-13 07:47:19 +0700

    Cleaning up

--- pages/shop.js
+++ pages/shop.js
@@ -9,61 +9,64 @@ function Shop({ products }) {
   return (
     <Fragment>
       <style jsx="true" global="true">{`
-                #shop {
-                    margin-top: 160px;
-                }
-                .products {
-                    display: grid;
-                    grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
-                    grid-gap: 24px;
-                }
-               #shop > ul {
-                    height: 60px;
-                    display: inline-block;
-                    background: #FF9E9E;
-                    color: #FFF;
-                    padding: 8px;
-                    padding-left: 16px;
-                    padding-right: 72px;
-                    position: fixed;
-                    top: 12%;
-               }
-               #shop span {
-                    background: #A9EEC2;
-                    padding: 0 8px;
-                    font-weight: 700;
-                    position: absolute;
-                    right: 16px;
-                    margin: 6px;
-                    font-size: 1.25rem;
-               }
-               #shop span:hover {
-                    cursor: pointer;
-               }
-               #shop ul li {
-                    margin: 8px 0;
-               }
-               #shop li {
-                    margin: 16px 0;
-                    font-size: 1.3rem;
-                    font-weight: 700;
-                    text-transform: uppercase;
-               }
-               .category-list-item {
-                    display: inline-block;
-                    transition: all .3s linear;
-                    opacity: 0;
-                    line-height: 2;
-               }
-           `}</style>
+        #shop {
+          margin-top: 160px;
+        }
+        .products {
+          display: grid;
+          grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+          grid-gap: 24px;
+        }
+        #shop > ul {
+          height: 60px;
+          display: inline-block;
+          background: #ff9e9e;
+          color: #fff;
+          padding: 8px;
+          padding-left: 16px;
+          padding-right: 72px;
+          position: fixed;
+          top: 12%;
+        }
+        #shop span {
+          background: #a9eec2;
+          padding: 0 8px;
+          font-weight: 700;
+          position: absolute;
+          right: 16px;
+          margin: 6px;
+          font-size: 1.25rem;
+        }
+        #shop span:hover {
+          cursor: pointer;
+        }
+        #shop ul li {
+          margin: 8px 0;
+        }
+        #shop li {
+          margin: 16px 0;
+          font-size: 1.3rem;
+          font-weight: 700;
+          text-transform: uppercase;
+        }
+        .category-list-item {
+          display: inline-block;
+          transition: all 0.3s linear;
+          opacity: 0;
+          line-height: 2;
+        }
+      `}</style>
       <Main title="CLOF | Product Page">
         <article id="shop">
           <ul style={{ height: isListExpanded ? "auto" : "60px" }}>
             <span onClick={() => setIsListExpanded(!isListExpanded)}>+</span>
             <li>All Products</li>
-            <ul className="category-list-item" style={{
-              opacity: isListExpanded ? "1" : "0"
-            }}>
+            <ul
+              className="category-list-item"
+              style={{
+                opacity: isListExpanded ? "1" : "0"
+              }}
+            >
               <li>Outerwear</li>
               <li>Accessories</li>
             </ul>
@@ -86,7 +89,7 @@ function Shop({ products }) {
 }
 
 Shop.getInitialProps = async ({ req }) => {
-  const res = await fetch("/api/products");
+  const res = await fetch(process.env.API_URL + "/api/products");
   const json = await res.json();
   return { products: json };
 };
